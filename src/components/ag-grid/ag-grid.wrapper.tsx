173858f15ccfbaf7ccb6e5988bf281ac65ab12f1
Yura Husak <yura.husak@juriba.com>
2020-12-10 18:55:25 +0200

    Change ag-grid dependencies versions, adde default context menu

--- src/components/ag-grid/ag-grid.wrapper.tsx
+++ src/components/ag-grid/ag-grid.wrapper.tsx
@@ -1,11 +1,13 @@
-import { AgGridColumn, AgGridReact } from "ag-grid-react";
+import { AgGridColumn, AgGridReact } from "@ag-grid-community/react";
 import React from "react";
 import grid from "../../api/grid";
-import 'ag-grid-enterprise';
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+import '@ag-grid-community/core/dist/styles/ag-grid.css';
+import '@ag-grid-community/core/dist/styles/ag-theme-alpine.css';
+import { AllModules, GetMainMenuItemsParams, Module, ModuleRegistry } from '@ag-grid-enterprise/all-modules';
 
+ModuleRegistry.registerModules(AllModules);
 export default class AgGridWrapper extends React.Component<{}, { rowData: any }> {
+	private readonly modules: Module[] = AllModules;
 	constructor(props: {}) {
 		super(props);
 		this.state = {
@@ -18,11 +20,19 @@ export default class AgGridWrapper extends React.Component<{}, { rowData: any }>
 		this.setState({ rowData: gridInfo$.data });
 	}
 
+	getMainMenuItems = (params: GetMainMenuItemsParams) => {
+		return params.defaultItems;
+	};
+
 	render() {
 		return (
 			<div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
 				<AgGridReact
-					rowData={this.state.rowData}>
+					modules={this.modules}
+					rowData={this.state.rowData}
+					allowContextMenuWithControlKey={true}
+					suppressContextMenu={false}
+					getMainMenuItems={params => this.getMainMenuItems(params)}>
 					<AgGridColumn field="make" sortable={true}></AgGridColumn>
 					<AgGridColumn field="model" sortable={true} filter={true}></AgGridColumn>
 					<AgGridColumn field="price" sortable={true} filter={true}></AgGridColumn>
